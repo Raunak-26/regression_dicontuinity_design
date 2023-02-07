fe6e06223bbb27861e4739e3199b205adfeebdaf
vanshajbindlish, Raunak-26 <vanshajbindlish01@gmail.com, raunakmehrotra2000@gmail.com>
2023-02-07 18:32:00 +0100

    deleted unsed files

--- src/rdd_rm/data_management/__init__.py
+++
@@ -1,4 +0,0 @@
-from rdd_rm.data_management.clean_data import clean_data
-
-
-__all__ = [clean_data]
--- src/rdd_rm/final/__init__.py
+++
@@ -1,4 +0,0 @@
-from rdd_rm.final.plot import plot_regression_by_age
-
-
-__all__ = [plot_regression_by_age]
